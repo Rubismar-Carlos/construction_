def1f55cdca27873ef24f513e70167ff6e0a5fb7
Rubismar Carlos <rubens_fake_05@hotmail.com>
2023-07-18 18:17:46 -0300

    Adiconando layout.js e deletando alguns arquivos que não serão nescessarios

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,20 @@
-import styles from './Navbar.module.css'
-
+import Link from "next/link"
 export default function Navbar() {
     return (
         <div>
-
+            <ul>
+                <li>
+                    <Link href={'/about'}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    hello
+                </li>
+                <li>
+                    hello
+                </li>
+            </ul>
         </div>
     )
 }
--- src/pages/About/About.js
+++
@@ -1,7 +0,0 @@
-export default function About() {
-    return (
-        <div>
-            <h2>Hello My name Is Rubismar Carlos</h2>
-        </div>
-    )
-}
