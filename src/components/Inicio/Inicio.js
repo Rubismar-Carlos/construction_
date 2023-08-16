cd988529c5134e1f35360aa4e9349005bcd47dde
Rubismar Carlos <rubens_fake_05@hotmail.com>
2023-08-16 19:14:13 -0300

    adicionando arquivos css e js do componente INICIO

---
+++ src/components/Inicio/Inicio.js
@@ -0,0 +1,12 @@
+'use cliente'
+
+// CSS
+import styles from '../../styles/Inicio.module.css'
+
+export default function Inicio() {
+    return (
+        <div className={styles.inicio_container}>
+
+        </div>
+    )
+} 
