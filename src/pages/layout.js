88ca49482fc7b1e6ef381bb4f3af3708a152ed15
Rubismar Carlos <rubens_fake_05@hotmail.com>
2023-08-16 19:13:14 -0300

    Adicionando mudanças no layout

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -2,12 +2,21 @@
 import Footer from "<prefix>/components/Footer/Footer";
 import Navbar from "<prefix>/components/Navbar/Navbar";
 
+// Style
+import styles from '../styles/Home.module.css'
+
 export default function Layout({ children }) {
     return (
-        <>
-            < Navbar />
-            {children}
-            < Footer />
-        </>
+        <div className={styles.layout_container}>
+            <div className={styles.layout_navbar}>
+                < Navbar />
+            </div>
+            <div className={styles.layout_main}>
+                {children}
+            </div>
+            <div className={styles.layout_footer}>
+                < Footer />
+            </div>
+        </div>
     )
 }
